3183da193a5299a45d411beb1e692f01cfad1967
saipriyasahoo228 <saipriyasahoo228@gmail.com>
2025-09-07 11:01:29 +0530

    Updates

---
+++ assets/Delete-cHl_EB0l.js
@@ -0,0 +1,1 @@
+import{j as t}from"./main-IxGEKZMA.js";import{s as o}from"./TableRow-CRAT5KT_.js";const r=o(t.jsx("path",{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z"}));export{r as D};
